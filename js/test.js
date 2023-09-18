77148b221b98d556ab7048a99292eb31e055750b
hill <songweihill@outlook.com>
2023-09-18 10:05:05 +0800

    add all

---
+++ js/test.js
@@ -0,0 +1,8 @@
+const myElement = document.getElementById("test1");
+const data = myElement.firstElementChild
+data.addEventListener("click", e => {
+
+data.textContent="clicked event and fucntion called"
+
+
+})
