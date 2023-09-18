a30b910a543fc72ebd9d6f12c2336c3298bc44eb
hill <songweihill@outlook.com>
2023-09-18 21:00:33 +0800

    added a subpage

--- js/test.js
+++
@@ -1,8 +0,0 @@
-const myElement = document.getElementById("test1");
-const data = myElement.firstElementChild
-data.addEventListener("click", e => {
-
-data.textContent="clicked event and fucntion called"
-
-
-})
