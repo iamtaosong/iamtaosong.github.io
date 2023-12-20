a57f615c313871f6285720ca8036cb12192dfeac
songweihill <songweihill@outlook.com>
2023-12-20 08:30:30 +0800

    fixed string comparsion

--- js/ipa/vowel/summary.js
+++ js/ipa/vowel/summary.js
@@ -320,7 +320,7 @@ function letterSoundCheck(e) {
 
   if(common.game=="wordImageMatch")
   {
-    if(learnObj.letterList[wordImageMatchObj.counter].answer.includes(e.target.value))
+    if(learnObj.letterList[wordImageMatchObj.counter].word === (e.target.value))
     {
         document.getElementById("showResult").innerHTML=common.goodResult;
     }else 
