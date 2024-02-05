e89d6df35fcee3b1f0cbcf48a69de4facf4a3ddc
songweihill <songweihill@outlook.com>
2024-02-05 08:50:47 +0800

    fixed family bugs

---
+++ js/grammar/basic/pronoun/summary.js
@@ -0,0 +1,26 @@
+"use strict"; 
+
+document.getElementById("video0").addEventListener("click",function(){
+
+  document.getElementById('videoLink').src ="https://www.youtube.com/embed/iSybXF9qou0?si=BiPpKMq5_FRplZb3";
+  
+});
+
+document.getElementById("video1").addEventListener("click",function(){
+
+  document.getElementById('videoLink').src ="https://www.youtube.com/embed/IXjfcEmqAHc?si=VZgeNmsZGErFzHzz";
+  
+});
+
+document.getElementById("video2").addEventListener("click",function(){
+
+  document.getElementById('videoLink').src ="https://www.youtube.com/embed/fLXBoTJwwX8?si=_y-QANKOpX3A4-mD";
+  
+});
+
+document.getElementById("video3").addEventListener("click",function(){
+
+  document.getElementById('videoLink').src ="https://www.youtube.com/embed/9BmvvC9qe2s?si=Z-CdRQxH_ptWxuUD";
+  
+});
+
